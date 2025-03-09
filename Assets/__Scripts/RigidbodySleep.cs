d0997a5bc1d987ab3dbb5cba00d0c55213bfdc80
Forclasses <brucec22180@gmail.com>
2025-03-08 23:28:31 -0600

    Stopping at pg 720
    Wall prefab made and rigibody sleep made as well

--- Assets/__Scripts/RigidbodySleep.cs
+++ Assets/__Scripts/RigidbodySleep.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
 
+
+[RequireComponent( typeof(Rigidbody))]
 public class RigidbodySleep : MonoBehaviour
 {
-    
+    private int sleepCountdown = 4;
+    private Rigidbody rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (sleepCountdown > 0) {
+            rigid.Sleep();
+            sleepCountdown--;
+        }
+    }
 }
